8010cc3e6385925b4f91ffde40ef1f6758c9af16
arnavjain2710 <119425235+arnavjain2710@users.noreply.github.com>
2024-03-22 14:52:05 +0530

    custom login page created

--- client/app/api/auth/[...nextauth]/options.tsx
+++ client/app/api/auth/[...nextauth]/options.tsx
@@ -3,6 +3,9 @@ import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 export const options: NextAuthOptions = {
+    pages: {
+        signIn: '/auth/signin',
+      },
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID as string,
@@ -37,8 +40,5 @@ export const options: NextAuthOptions = {
         })
     ],
 
-  pages: {
-    signIn: '/auth/signin',
 
-  }
 }
--- client/app/signin/form.tsx
+++ client/app/signin/form.tsx
@@ -12,8 +12,8 @@ export const LoginForm = () => {
   });
   const [error, setError] = useState("");
 
-  const searchParams = useSearchParams();
-  const callbackUrl =  searchParams.get("callbackUrl") || "/";
+  // const searchParams = useSearchParams();
+  // const callbackUrl =  "/";
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     // e.preventDefault();
@@ -94,7 +94,7 @@ export const LoginForm = () => {
         <a
           className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
           style={{ backgroundColor: "#3b5998" }}
-          onClick={() => signIn("google" , {callbackUrl})}
+          onClick={() => signIn("google")}
           role="button"
         >
           <img
