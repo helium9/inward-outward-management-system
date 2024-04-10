1bbb7dbd25218be5b5c1446323742e0fb856cf7d
Aditya Kshitiz <adityakshitiz2017@gmail.com>
2024-04-11 02:38:25 +0530

    frontend of claimInfo

--- client/app/claimInfo/[id]/page.tsx
+++ client/app/claimInfo/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   Table,
   TableBody,
@@ -11,7 +12,7 @@ import {
 import FilterDialog from "@/components/FilterDialog";
 import { Button } from "@/components/ui/button";
 import { CopyIcon } from "@radix-ui/react-icons";
-import { Input } from "@/components/ui/input";
+import { Input } from "@nextui-org/react";
 import { Label } from "@/components/ui/label";
 import { AssignmentOutlined } from "@mui/icons-material";
 import { useEffect, useState } from "react";
@@ -26,6 +27,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import { Textarea } from "@/components/ui/textarea";
 import {
   Dialog,
@@ -37,19 +47,86 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-
+import {
+  Phone,
+  MailOutline,
+  Search,
+  FilterAltOutlined,
+  ExpandCircleDownOutlined,
+  FileOpenOutlined,
+  PostAddOutlined,
+  TaskOutlined,
+  ContentPasteGoOutlined,
+  ArrowOutward,
+} from "@mui/icons-material";
 const filterLabels = {
-  inward_number:"Invoice number",
+  inward_number: "Invoice number",
   issue_date: "Issue Date",
-  ind_name:"Indentor name",
-  dept_name:"Department name",
-  party_name:"Party name",
-  claimant_name:"Claimant name",
-  subject:"Subject",
-  amount:"Amount",
-  status:"Status",
-  alloted_to_name:"Alloted to"
-}
+  ind_name: "Indentor name",
+  dept_name: "Department name",
+  party_name: "Party name",
+  claimant_name: "Claimant name",
+  subject: "Subject",
+  amount: "Amount",
+  status: "Status",
+  alloted_to_name: "Alloted to",
+};
+
+const ClaimRow = ({
+  action,
+  takenBy,
+  remarks,
+  dateTime,
+  expanded,
+  onClick,
+}: {
+  action: string;
+  takenBy: string;
+  remarks: string;
+  dateTime: string;
+  expanded: boolean;
+}) => {
+  const iconMap = {
+    forward: <ContentPasteGoOutlined sx={{ fontSize: 36, marginRight: 0.5 }} />,
+    outward: <TaskOutlined sx={{ fontSize: 40 }} />,
+    inward: <FileOpenOutlined sx={{ fontSize: 40 }} />,
+    new: <PostAddOutlined sx={{ fontSize: 40 }} />,
+    stage: <AssignmentOutlined sx={{ fontSize: 40 }} />,
+  };
+  const icon = iconMap[action] || null;
+  //adding margin or padding in any table component doesn't work.
+  return (
+    <TableRow onClick={onClick}>
+      <TableCell className="font-medium">
+        <div className="ml-6 my-2">
+          <div className="flex flex-row items-center">
+            {icon}
+            <div className="flex flex-col ml-2 font-semibold">{action}</div>
+          </div>
+        </div>
+      </TableCell>
+      <TableCell>
+        <p className="my-2 font-semibold">{takenBy}</p>
+      </TableCell>
+      <TableCell>
+        <p
+          className={`my-2 font-semibold ${
+            !expanded && `overflow-hidden h-10 text-ellipsis`
+          }`}
+        >
+          {remarks}
+        </p>
+      </TableCell>
+      <TableCell>
+        <div className="flex flex-col gap-2">
+          <p>{dateTime}</p>
+          {expanded && <u>Revert Changes?</u>}
+        </div>
+      </TableCell>
+    </TableRow>
+  );
+};
+
 function Page({ params }: { params: { id: string } }) {
   const { data: session, status } = useSession();
   const [claimData, setClaimData] = useState({
@@ -118,6 +195,10 @@ function Page({ params }: { params: { id: string } }) {
       alloted_to_name: "",
     });
     setDate(undefined);
+  };
+  const [expanded, setExpanded] = useState(false); //this is a single variable that'll be matched to the key in array map to decide which row is expanded.
+  const handleExpandHistory = () => {
+    setExpanded((expanded) => !expanded);
   };
   return (
     <main className="p-2 px-4 sm:px-10 xl:px-24">
@@ -202,10 +283,15 @@ function Page({ params }: { params: { id: string } }) {
           </Table>
         </div>
       </section>
-      <section>
+      <section className="py-4 flex flex-row justify-end">
         <Dialog>
           <DialogTrigger asChild>
-            <Button variant="outline">Actions</Button>
+            <Button
+              variant="outline"
+              className="w-32 flex flex-row gap-1 font-semibold bg-gradient-to-b from-zinc-100 to-zinc-300"
+            >
+              Actions <ArrowOutward sx={{ fontSize: 20 }} />
+            </Button>
           </DialogTrigger>
           <DialogContent className="sm:max-w-md">
             <DialogHeader>
@@ -229,11 +315,15 @@ function Page({ params }: { params: { id: string } }) {
               </SelectContent>
             </Select>
             <Label htmlFor="remarks">Remarks</Label>
-            <Textarea placeholder="Type your remarks here." id="remarks" />
-            <DialogFooter className="sm:justify-start">
+            <Textarea
+              placeholder="Type your remarks here."
+              id="remarks"
+              className="h-36"
+            />
+            <DialogFooter className="sm:justify-end">
               <DialogClose asChild>
                 <Button type="button" variant="secondary">
-                  Close
+                  Submit
                 </Button>
               </DialogClose>
             </DialogFooter>
@@ -241,15 +331,105 @@ function Page({ params }: { params: { id: string } }) {
         </Dialog>
       </section>
       <section>
-        <FilterDialog
-          date={date}
-          setDate={setDate}
-          filterData={filterData}
-          setFilterData={setFilterData}
-          handleFilterApply={handleFilterApply}
-          handleFilterClear={handleFilterClear}
-          labels={filterLabels}
-        />
+        <div className="my-4 flex flex-row items-center">
+          <p className="text-2xl font-bold">Action History</p>
+          <div className="flex flex-row w-full ml-auto gap-3 max-w-unit-8xl">
+            <div className="flex items-center min-w-40 space-x-2">
+              <Checkbox id="terms" />
+              <label
+                htmlFor="terms"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Show current user
+              </label>
+            </div>
+            <FilterDialog
+              date={date}
+              setDate={setDate}
+              filterData={filterData}
+              setFilterData={setFilterData}
+              handleFilterApply={handleFilterApply}
+              handleFilterClear={handleFilterClear}
+              labels={filterLabels}
+            />
+            <Input
+              size="sm"
+              variant="bordered"
+              classNames={{
+                base: `m-0`,
+                inputWrapper: "rounded border-zinc-200 border",
+              }}
+              placeholder={`Search`}
+              startContent={<Search sx={{ color: "#999999" }} />}
+            />
+          </div>
+        </div>
+        <div className="rounded-md border border-zinc-300">
+          <Table>
+            <TableHeader>
+              <TableRow className="text-zinc-500 font-semibold">
+                <TableHead className="w-28">
+                  <p className="ml-6">Action</p>
+                </TableHead>
+                <TableHead className="w-36">Taken by</TableHead>
+                <TableHead className="w-80">Remarks</TableHead>
+                <TableHead className="w-36">
+                  <p className="mr-6">Time</p>
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <ClaimRow
+                action="forward"
+                takenBy="Aditya Kshitiz"
+                remarks="Received a reimbursement request for a life-size cardboard cutout of the  boss. Approved, but let's hope it doesn't start making executive  decisions around here! Received a reimbursement request for a life-size cardboard cutout of the  boss. Approved, but let's hope it doesn't start making executive  decisions around here!"
+                dateTime="10-03-24 15:03"
+                expanded={expanded}
+                onClick={handleExpandHistory}
+              />
+              <ClaimRow
+                action="forward"
+                takenBy="Aditya Kshitiz"
+                remarks="Received a reimbursement request for a life-size cardboard cutout of the  boss. Approved, but let's hope it doesn't start making executive  decisions around here! Received a reimbursement request for a life-size cardboard cutout of the  boss. Approved, but let's hope it doesn't start making executive  decisions around here!"
+                dateTime="10-03-24 15:03"
+                expanded={false}
+              />
+              <ClaimRow
+                action="forward"
+                takenBy="Aditya Kshitiz"
+                remarks="Received a reimbursement request for a life-size cardboard cutout of the  boss. Approved, but let's hope it doesn't start making executive  decisions around here! Received a reimbursement request for a life-size cardboard cutout of the  boss. Approved, but let's hope it doesn't start making executive  decisions around here!"
+                dateTime="10-03-24 15:03"
+                expanded={false}
+              />
+            </TableBody>
+          </Table>
+        </div>
+      </section>
+      <section className="my-4">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                2
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
       </section>
     </main>
   );
